39b2df56d431ab1349ff8e7b3b5e591c384aa20e
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-06-24 00:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function insert_Row() {
-  let tr = document.createElement("tr");
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("insertBtn").addEventListener("click", function () {
+    let tr = document.createElement("tr");
 
-  let td1 = document.createElement("td");
-  td1.innerText = "New Cell1";
+    let td1 = document.createElement("td");
+    td1.innerText = "New Cell1";
 
-  let td2 = document.createElement("td");
-  td2.innerText = "New Cell2";
+    let td2 = document.createElement("td");
+    td2.innerText = "New Cell2";
 
-  tr.append(td1);
-  tr.append(td2);
+    tr.append(td1);
+    tr.append(td2);
 
-  let table = document.getElementById("sampleTable");
-  table.prepend(tr);
-}
-
-window.insert_Row = insert_Row;
+    let table = document.getElementById("sampleTable");
+    table.prepend(tr);
+  });
+});
