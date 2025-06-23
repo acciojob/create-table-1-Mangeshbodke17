b82ca3e626b617c8a3abfddf675291b80cebc6f6
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-06-24 00:39:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  let tr = document.createElement("tr");
+
+  let td1 = document.createElement("td");
+  td1.innerText = "New Cell1";
+
+  let td2 = document.createElement("td");
+  td2.innerText = "New Cell2";
+
+  tr.append(td1);
+  tr.append(td2);
+
+  // Insert the row at the top of the table
+  let table = document.getElementById("sampleTable");
+  table.prepend(tr);
 }
